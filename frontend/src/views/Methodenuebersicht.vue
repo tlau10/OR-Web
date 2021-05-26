68ae7a9bb2911bf93637cf0952beca219ce09de8
Julia Hansi <ju851han@htwg-konstanz.de>
2021-05-26 15:08:49 +0200

    added further entries in table

--- frontend/src/views/Methodenuebersicht.vue
+++ frontend/src/views/Methodenuebersicht.vue
@@ -1,48 +1,153 @@
 <template>
-  <v-card
-    flat
-    tile
-  >
-       <v-breadcrumbs
-      :items="items"
-      large
-    ></v-breadcrumbs>
-
   <v-simple-table>
     <template v-slot:default>
       <thead>
         <tr>
-          <th class="text-left">
-            Name
-          </th>
-          <th class="text-left">
-            Kurzbeschreibung
-          </th>
-          <th class="text-center">
-            technische Dokumentation
-          </th>
-          <th class="text-center">
-            Benutzerhandbuch
-          </th>
-          <th class="text-center">
-              Download
-              </th>
+          <th class="text-left">Kategorie</th>
+          <th class="text-left">Name</th>
+          <th class="text-left">Kurzbeschreibung</th>
+          <th class="text-center">technische Dokumentation</th>
+          <th class="text-center">Benutzerhandbuch</th>
+          <th class="text-center">Download</th>
         </tr>
       </thead>
       <tbody>
-      <!--  <tr
+        <!--  <tr
           v-for="item in methoden"
           :key="item.name"
         >-->
         <tr>
-          <td>Paket Transport Simulation System 1.1 (PTSS)</td>
-          <td>bla bla bla bla</td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td class="text-center">Transportplanung</td>
+          <td class="text-left">
+            Paket Transport Simulation System 1.1 (PTSS)
+          </td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Transportplanung</td>
+          <td class="text-left">TR-Optimizer 2.1</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Transportplanung</td>
+          <td class="text-left">BOP - Beladungsoptimierung 1.1</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Standortplanung</td>
+          <td class="text-left">Eisenstadt 1.4</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Standortplanung</td>
+          <td class="text-left">innerbetriebliche Standortplanung 1.1</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Standortplanung</td>
+          <td class="text-left">Standortplanung 3.0</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Zuordnungsplanung 2.1</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Lineare Portfolio Optimierung 2.0</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Roundtrip 0.9</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+          >
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Job Shop 2.2</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Verkehrsplaner</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Maschinenbelegungsplanung 5.2</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">OP-Scheduling 2.3</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Ernährungsplaner 2.0</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Einkaufswagen</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
+        </tr>
+        <tr>
+          <td class="text-center">Personalplanung</td>
+          <td class="text-left">Hotelmanager 5.0</td>
+          <td class="text-left"></td>
+          <td class="text-center"><v-icon>mdi-file-cog-outline</v-icon></td>
+          <td class="text-center"><v-icon>mdi-file-account</v-icon></td>
+          <td class="text-center"><v-icon>mdi-download</v-icon></td>
         </tr>
       </tbody>
     </template>
   </v-simple-table>
-  </v-card>
 </template>
