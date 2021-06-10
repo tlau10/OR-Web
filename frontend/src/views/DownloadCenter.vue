aafb46584082af817761e64059fd02ae70e137a5
Julia Hansi <ju851han@htwg-konstanz.de>
2021-06-10 14:16:46 +0200

    added view

---
+++ frontend/src/views/DownloadCenter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <v-breadcrumbs :items="items" large></v-breadcrumbs>
+    </div>
+    
+    </template>
+
+    <script>
+export default {
+  data: () => ({
+    items: [
+      {
+        text: "Home",
+        disabled: false,
+        href: "/",
+      },
+      {
+        text: "DownloadCenter",
+        disabled: true,
+      },
+    ],
+  }),
+};
+</script>
