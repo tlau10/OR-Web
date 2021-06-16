c1df3414a2329777a5dded327c8edd38ba15d567
Julia Hansi <ju851han@htwg-konstanz.de>
2021-06-16 20:34:42 +0200

    added start + download links

--- frontend/src/views/Methodenuebersicht_v2.vue
+++ frontend/src/views/Methodenuebersicht_v2.vue
@@ -1,35 +1,32 @@
 <template>
   <div class="about">
     <v-breadcrumbs :items="items" large></v-breadcrumbs>
-     <template>
-    <v-data-table :headers="headers"
-      :items="method"
-      :items-per-page="20"
-      sort-by="name"
-      group-by="Kategorie"
-      class="elevation-1"
+    <template>
+      <v-data-table
+        :headers="headers"
+        :items="method"
+        :items-per-page="20"
+        sort-by="name"
+        group-by="Kategorie"
+        class="elevation-1"
       >
-      <template v-slot:[`item.name`]="{ item }">
- <v-btn
-                :to="item.start"
-                depressed
-                plain
-                class="text-capitalize"
-                >{{ item.name }}</v-btn
-              >
-      </template>
-      <template v-slot:[`item.start`]="{ item }">
-      <v-btn text icon :to="item.start">
-                <v-icon>mdi-information-outline</v-icon>
-              </v-btn>
-      </template>
-      <template v-slot:[`item.download`]="{ item }">
-            <v-btn text icon :to="item.download">
-                <v-icon>mdi-download</v-icon>
-              </v-btn>
-      </template>
+        <template v-slot:[`item.name`]="{ item }">
+          <v-btn :to="item.start" depressed plain class="text-capitalize">{{
+            item.name
+          }}</v-btn>
+        </template>
+        <template v-slot:[`item.start`]="{ item }">
+          <v-btn text icon :to="item.start">
+            <v-icon>mdi-information-outline</v-icon>
+          </v-btn>
+        </template>
+        <template v-slot:[`item.download`]="{ item }">
+          <v-btn text icon :to="item.download">
+            <v-icon>mdi-download</v-icon>
+          </v-btn>
+        </template>
       </v-data-table>
-     </template>
+    </template>
   </div>
 </template>
 <script>
@@ -47,17 +44,17 @@ export default {
       },
     ],
     headers: [
-      { text: "Methodenname", align: "start", value: "name" , groupable: false, },
+      { text: "Methodenname", align: "start", value: "name", groupable: false },
       { text: "Kategorie", align: "left", value: "Kategorie" },
       { text: "Details", align: "center", value: "start", sortable: false },
-      { text: "Download", align: "center", value: "download" , sortable: false},
+      { text: "Download", align: "center", value: "download", sortable: false },
     ],
     method: [
       {
         name: "Wagner-Whitin-LP 1.3",
         Kategorie: "Bestellmengenplanung",
-        start: "",
-        download: "",
+        start: "Wagner-Whitin-LP",
+        download: "DownloadCenter#Wagner-Whitin-LP",
       },
       {
         name: "Wagner-Whitin 1.2",
@@ -68,98 +65,98 @@ export default {
       {
         name: "TR-Optimizer 2.1",
         Kategorie: "Transportplanung",
-        start: "",
-        download: "",
+        start: "TR-Optimizer",
+        download: "DownloadCenter#TR-Optimizer",
       },
       {
         name: "PTSS - Paket Transport Simulation System 1.1",
         Kategorie: "Transportplanung",
         start: "PTSS",
-        download: "Methodenuebersicht#PTSS",
+        download: "DownloadCenter#PTSS",
       },
       {
         name: "BOP - Beladungsoptimierung 1.1",
         Kategorie: "Transportplanung",
-        start: "",
-        download: "",
+        start: "BOP",
+        download: "DownloadCenter#BOP",
       },
       {
         name: "Eisenstadt 1.4",
         Kategorie: "Standortplanung",
-        start: "",
-        download: "",
+        start: "Eisenstadt",
+        download: "DownloadCenter#Eisenstadt",
       },
       {
         name: "innerbetriebliche Standortplanung 1.1",
         Kategorie: "Standortplanung",
-        start: "",
-        download: "",
+        start: "innerbetrieblicheStandortplanung",
+        download: "DownloadCenter#innerbetrieblicheStandortplanung",
       },
       {
         name: "Standortplanung 3.0",
         Kategorie: "Standortplanung",
-        start: "",
-        download: "",
+        start: "Standortplanung",
+        download: "DownloadCenter#Standortplanung",
       },
       {
         name: "Zuordnungsplanung 2.1",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "Zuordnungsplanung",
+        download: "DownloadCenter#Zuordnungsplanung",
       },
       {
         name: "Lineare Portfolio Optimierung 2.0",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "Portfolio",
+        download: "DownloadCenter#Portfolio",
       },
       {
         name: "Roundtrip 0.9",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "Roundtrip",
+        download: "DownloadCenter#Roundtrip",
       },
       {
         name: "Job Shop 2.2",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "JobShop",
+        download: "DownloadCenter#JobShop",
       },
       {
         name: "Verkehrsplaner",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "Verkehrsplaner",
+        download: "DownloadCenter#Verkehrsplaner",
       },
       {
         name: "Maschinenbelegungsplanung 5.2",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "Maschinenbelegungsplanung",
+        download: "DownloadCenter#Maschinenbelegungsplanung",
       },
       {
         name: "OP-Scheduling 2.3",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "OP-Scheduling",
+        download: "DownloadCenter#OP-Scheduling",
       },
       {
         name: "Ernährungsplaner 2.0",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "Ernährungsplaner",
+        download: "DownloadCenter#Ernährungsplaner",
       },
       {
         name: "Einkaufswagenoptimierung 1.0",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "Einkaufswagenoptimierung",
+        download: "DownloadCenter#Einkaufswagenoptimierung",
       },
       {
         name: "Hotelmanager 5.0",
         Kategorie: "Personalplanung",
-        start: "",
-        download: "",
+        start: "Hotelmanager",
+        download: "DownloadCenter#Hotelmanager",
       },
     ],
   }),
