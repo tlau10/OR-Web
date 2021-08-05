e297aa2ca18d42d1152cec8f779b311ad449a0a9
Julia Hansi <ju851han@htwg-konstanz.de>
2021-08-05 12:01:36 +0200

    add function for adding table

--- frontend/src/views/Methods/WagnerWhitinExecute.vue
+++ frontend/src/views/Methods/WagnerWhitinExecute.vue
@@ -9,23 +9,32 @@
 
     <body>
       <h1>Wagner Whitin 1.2 - Algorithmus</h1>
-      <label for="ruestkosten">Rüstkosten</label>
-      <input id="ruestkosten" name="ruestkosten" type="number" min="1" />
-      <label for="anzPerioden">Anzahl der Perioden</label>
-      <input id="anzPerioden" name="anzPerioden" type="number" />
+      <div>
+        <label for="bestellkostensatz">Bestellkostensatz</label>
+        <input
+          id="bestellkostensatz"
+          name="bestellkostensatz"
+          type="number"
+          min="1"
+        />
+        <span>GE</span> <br />
+      </div>
+      <div>
+        <label for="anzPerioden">Anzahl der Perioden</label>
+        <input id="anzPerioden" name="anzPerioden" type="number" /><span
+          >Perioden</span
+        >
+      </div>
+      <div>
+        <label for="lagerkostensatz">Lagerkostensatz</label>
+        <input id="lagerkostensatz" name="lagerkostensatz" type="number" /><span
+          >GE pro ME pro ZE</span
+        >
+        <!-- TODO Kontrollkästchen variable kosten-->
+      </div>
+      <button id="btnCreateTbl">Erstelle Tabelle</button>
+      <div id="tbl"></div>
 
-      <table>
-        <tr>
-          <th>Periode</th>
-          <th>Bedarf</th>
-          <th>Lagerkosten</th>
-        </tr>
-        <tr>
-          <td><input id="period" name="period" type="number" min="1" /></td>
-          <td><input id="bedarf" name="bedarf" type="number" /></td>
-          <td><input id="lagerkosten" name="lagerkosten" type="number" /></td>
-        </tr>
-      </table>
       <input id="start" type="button" value="Speichern" />
       <div id="output"></div>
     </body>
@@ -34,14 +43,17 @@
 <script  type = "text/javascript" >
 var start;
 var output;
+var createTbl;
 /**
  * init() wird aufgerufen, sobald die Seite geladen ist, weist den Variablen die ID zu und setzt den EventListener auf den Button mit der ID "start"
  */
 function init() {
   start = document.getElementById("start");
   output = document.getElementById("output");
+  createTbl = document.getElementById("btnCreateTbl");
   //Sobald der Button mit der ID "start" angeklickt wird, wird die Funktion socket() aufgerufen
   start.addEventListener("click", socket, false);
+  createTbl.addEventListener("click", createTable, false);
 }
 /**
  * socket() erstellt eine Verbindung zum WebSocket, holt Daten aus den input-Feldern und leitet diese an den WebSocket weiter
@@ -69,6 +81,31 @@ function writeToScreen(message) {
   pre.innerHTML = message;
   output.appendChild(pre);
 }
+
+function createTable() {
+  var tbl =
+    "<div id=\"tbl\"><table><tr><th>Periode</th><th>Bedarf der Periode</th><th>Lagerkosten der Periode</th></tr>";
+  var anzPerioden = document.getElementById("anzPerioden").value;
+  var lagerkostensatz = document.getElementById("lagerkostensatz").value;
+  console.log('erstelle tbody');
+  var i = 1;
+  for (i; i <= anzPerioden ; i++) {
+    console.log(i);
+    tbl +=
+      "<tr><td>" +
+      i +
+      "</td><td><input id=\"bedarf"+i+"\" name=\"bedarf"+i+"\" type=\"number\" /></td><td>" +
+      lagerkostensatz +
+      "</td></tr>";
+      console.log(i+". Runde: "+tbl)
+  } 
+  tbl += "</table></div>";
+  var divTbl = document.getElementById("tbl");
+  var create = document.createElement(divTbl); //TODO Error
+  divTbl.replaceWith(create);
+
+}
+
 /**
  * Ruft init() Funktion auf, sobald die Seite geladen ist
  */
