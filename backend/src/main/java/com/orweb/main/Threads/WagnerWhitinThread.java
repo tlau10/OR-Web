a9adc945eb930fb73556bdd0f40b232b19b72c50
Julia Hansi <ju851han@htwg-konstanz.de>
2021-08-10 19:11:03 +0200

    parse string array to number array

--- backend/src/main/java/com/orweb/main/Threads/WagnerWhitinThread.java
+++ backend/src/main/java/com/orweb/main/Threads/WagnerWhitinThread.java
@@ -1,6 +1,6 @@
 package com.orweb.main.Threads;
 
-import java.util.Scanner;
+import java.util.regex.PatternSyntaxException;
 
 import com.orweb.main.logic.Methods.WagnerWhitin;
 
@@ -9,10 +9,9 @@ import org.springframework.web.socket.WebSocketSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class WagnerWhitinThread extends Thread {
-    
-	private static final Logger LOGGER = LoggerFactory.getLogger(WagnerWhitinThread.class);
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WagnerWhitinThread.class);
     private WebSocketSession webSocketSession;
     private TextMessage message;
 
@@ -23,14 +22,32 @@ public class WagnerWhitinThread extends Thread {
 
     public void run() {
         String txt = message.getPayload();
-        LOGGER.info("message im WagnerWhitinThread" + txt );
+        LOGGER.info("message im WagnerWhitinThread" + txt);
         String[] str = txt.split(";");
         WagnerWhitin instance = new WagnerWhitin();
         instance.setAnzahlPerioden(Integer.parseInt(str[0]));
-        instance.setRuestkosten(Integer.parseInt(str[1]));
-        instance.setLagerkosten(str[2])); //TODO
-        instance.setPeriodenbedarf(str[3].split(" ")); //TODO
+        instance.setRuestkosten(Integer.parseInt(str[1])); // = bestellkostensatz
+        instance.setPeriodenbedarf(parseStringArrayToIntArray(str[2].split(" ")));
+        instance.setLagerkosten(parseStringArrayToDoubleArray(str[3].split(" ")));
         instance.start();
     }
 
+    private int[] parseStringArrayToIntArray(String[] strArray) {
+        int[] intArray = new int[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            LOGGER.info("Runde: " + i);
+            intArray[i] = Integer.parseInt(strArray[i]);
+        }
+        return intArray;
+    }
+
+    private double[] parseStringArrayToDoubleArray(String[] strArray) {
+        double[] doubleArray;
+        doubleArray = new double[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            LOGGER.info("Runde: " + i);
+            doubleArray[i] = Integer.parseInt(strArray[i]);
+        }
+        return doubleArray;
+    }
 }
