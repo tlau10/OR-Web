64b1449bc97226c972fe27d2d9c9202e2676d6a5
Julia Hansi <ju851han@htwg-konstanz.de>
2021-08-11 12:12:06 +0200

    edit parseStringArrayToDoubleArray function + add responseMsg

--- backend/src/main/java/com/orweb/main/Threads/WagnerWhitinThread.java
+++ backend/src/main/java/com/orweb/main/Threads/WagnerWhitinThread.java
@@ -1,7 +1,6 @@
 package com.orweb.main.Threads;
 
-import java.util.regex.PatternSyntaxException;
-
+import java.io.IOException;
 import com.orweb.main.logic.Methods.WagnerWhitin;
 
 import org.springframework.web.socket.TextMessage;
@@ -22,32 +21,60 @@ public class WagnerWhitinThread extends Thread {
 
     public void run() {
         String txt = message.getPayload();
-        LOGGER.info("message im WagnerWhitinThread" + txt);
         String[] str = txt.split(";");
         WagnerWhitin instance = new WagnerWhitin();
+        String responseMsg;
+
         instance.setAnzahlPerioden(Integer.parseInt(str[0]));
         instance.setRuestkosten(Integer.parseInt(str[1])); // = bestellkostensatz
         instance.setPeriodenbedarf(parseStringArrayToIntArray(str[2].split(" ")));
-        instance.setLagerkosten(parseStringArrayToDoubleArray(str[3].split(" ")));
+        instance.setLagerkosten(parseStringArrayToDoubleArray(str[3].split(" "), Integer.parseInt(str[0])));
         instance.start();
+
+        responseMsg = parseObjectArrayToString(instance.getObjectAusgabe())+ ";"+ instance.getMinGesamtKosten();
+        TextMessage response = new TextMessage(responseMsg);
+        LOGGER.info("Text"+ responseMsg);
+        try {
+            synchronized(webSocketSession) {//TODO
+                webSocketSession.sendMessage(new TextMessage(response));
+            }
+        } catch (IOException e) {
+            LOGGER.error("Send Response failed",e);
+        }
     }
 
     private int[] parseStringArrayToIntArray(String[] strArray) {
         int[] intArray = new int[strArray.length];
         for (int i = 0; i < strArray.length; i++) {
-            LOGGER.info("Runde: " + i);
             intArray[i] = Integer.parseInt(strArray[i]);
         }
         return intArray;
     }
 
-    private double[] parseStringArrayToDoubleArray(String[] strArray) {
+    private double[] parseStringArrayToDoubleArray(String[] strArray, int anzPerioden) {
         double[] doubleArray;
-        doubleArray = new double[strArray.length];
-        for (int i = 0; i < strArray.length; i++) {
-            LOGGER.info("Runde: " + i);
-            doubleArray[i] = Integer.parseInt(strArray[i]);
+        if(strArray.length > 1){
+            doubleArray = new double[strArray.length];
+            for (int i = 0; i < doubleArray.length; i++) {
+                doubleArray[i] = Double.parseDouble(strArray[i]);
+            }  
+        }else{
+            doubleArray = new double[anzPerioden];
+            for (int i = 0; i < doubleArray.length; i++) {
+                doubleArray[i] = Double.parseDouble(strArray[0]);
+            }
         }
         return doubleArray;
+    }
+
+    private String parseObjectArrayToString( Object[][] objectAusgabe){
+        String str="";
+        for (int i = 0; i < objectAusgabe.length; i++) {
+            for (int j = 0; j < objectAusgabe[i].length; j++) {
+                str += objectAusgabe[i][j] + " ";
+            }
+            str +="--";
+        }
+        return str;
     }
 }
