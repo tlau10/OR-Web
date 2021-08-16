a07c3533e6e1ddec5112bc4c8813b97bba6bb5ee
Maurice Gutmann <maurice.gutmann@gmx.e>
2021-08-16 18:36:28 +0200

    smaller changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
   {
     path: '/about',
